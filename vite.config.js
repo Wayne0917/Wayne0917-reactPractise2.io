49400d63119843cbb8abc82d2953e023c276a0c4
yappy99123@gmail.com <yappy99123@gmail.com>
2026-01-05 17:55:34 +0800

    Fix:修改 index 路徑 及 vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "",
+  base: "/Wayne0917-reactPractise2.io/",
   plugins: [react()],
-})
+});
